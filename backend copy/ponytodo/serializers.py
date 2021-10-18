97ee80cf38e7dfbf6e483b866dfbbf81f107cdbc
SporoZoite <lisavanhooydonk@gmail.com>
2021-10-18 15:57:57 +0200

    corr path

--- backend copy/ponytodo/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Ponytodo
-
-class PonytodoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Ponytodo
-        fields = ('id', 'title', 'description', 'completed')
